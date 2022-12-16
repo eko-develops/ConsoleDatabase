a2e5eeafb0657da99acbe7e5bbd04d9ddec2e33a
ekodevelops <ekodevelops@gmail.com>
2022-12-16 12:19:07 -0800

    Create connection class

---
+++ ConsoleDatabase/Connection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+
+namespace ConsoleDatabase
+{
+    class Connection
+    {
+
+        SqliteConnection connection;
+
+        public Connection(string dataSource)
+        {
+            connection = new SqliteConnection(dataSource);    
+        }
+
+        public void Open()
+        {
+            this.connection.Open();
+        }
+    }
+}
